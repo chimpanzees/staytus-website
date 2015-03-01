03fd93ac28b7f5f55e38b475c837aa7f310a0d68
Adam Cooke <adam@atechmedia.com>
2015-03-01 22:47:11 +0000

    link to api docs

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'demo' => redirect("http://demo.staytus.co", :status => 302)
   get 'repo' => redirect('http://github.com/adamcooke/staytus', :status => 302)
   get 'bug' => redirect('https://github.com/adamcooke/staytus/issues/new?labels=bug', :status => 302)
+  get 'api' => redirect('https://github.com/adamcooke/staytus/tree/master/doc/api', :status => 302)
   get 'blog' => redirect('http://blog.staytus.co', :status => 302)
   get 'donate' => redirect("/", :status => 302)
   root 'pages#home'
