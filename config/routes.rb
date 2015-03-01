0ee19da7dd2406b9cde575d52b6be985aac5ae2b
Adam Cooke <adam@atechmedia.com>
2015-03-01 19:38:37 +0000

    add some redirects from the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'repo' => redirect('http://github.com/adamcooke/staytus', :status => 302)
+  get 'bug' => redirect('https://github.com/adamcooke/staytus/issues/new?labels=bug', :status => 302)
+  get 'donate' => redirect("/", :status => 302)
   root 'pages#home'
 end
