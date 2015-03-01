fec0f697b88812b8ac0a1e64378406391cb32841
Adam Cooke <adam@atechmedia.com>
2015-03-01 19:39:12 +0000

    add demo redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'demo' => redirect("http://demo.staytus.co", :status => 302)
   get 'repo' => redirect('http://github.com/adamcooke/staytus', :status => 302)
   get 'bug' => redirect('https://github.com/adamcooke/staytus/issues/new?labels=bug', :status => 302)
   get 'donate' => redirect("/", :status => 302)
