0f033e3391fd55be5640415e2f571c3c9e0cf184
Adam Cooke <adam@atechmedia.com>
2015-03-03 14:26:24 +0000

    set the correct scope needed (still not ideal)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,5 @@ require 'omniauth'
 require 'omniauth-github'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], :scope => ''
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], :scope => 'public_repo'
 end
